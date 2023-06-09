cb723414017c40fee8d218789071529b786f0ee2
Александр <hyperpositron@gmail.com>
2023-06-09 19:17:33 +0200

    first commit

---
+++ src/main/java/ru/questions/randomQuestions/RandomQuestionsApplication.java
@@ -0,0 +1,13 @@
+package ru.questions.randomQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQuestionsApplication.class, args);
+	}
+
+}
